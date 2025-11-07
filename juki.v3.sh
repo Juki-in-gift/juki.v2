1fcfe416a3ab834a3d6cb8a278678300ce1fdb62
Juki-in-gift <kirakov700@gmail.com>
2025-11-07 21:08:12 +0300

    Update juki.v3.sh

--- juki.v3.sh
+++ juki.v3.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
-# Игнорировать Ctrl+C и Ctrl+Z
 trap '' SIGINT SIGTSTP
-
 while true; do
     clear
-    # Центрирование текста
-    lines=$(tput lines)
-    cols=$(tput cols)
-    line_pos=$((lines/2))
-    col_pos=$((cols/2-8))
-    
-    printf "\e[%d;%df\033[1;31mTEXT\033[0m" "$line_pos" "$col_pos"
+    # echo -e "\033[1;31mЖУКИ НА ВАШЕМ УСТРОЙСТВЕ\033[0m"
+    printf "\e[$(($(tput lines)/2));$(($(tput cols)/2-8))f\033[1;31mTEXT\033[0m"
     sleep 0.5
     clear
     sleep 0.5
